a12771c98c7f094762e138d5cf0ced26a436807a
Christopher Peach <000868225@tafesa.edu.au>
2014-03-23 10:31:06 +1030

    added jquery scroll elements

---
+++ js/scrollTo.js
@@ -0,0 +1,25 @@
+$("#info").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about-hud").offset().top
+    }, 2000);
+});
+$("#features").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#feature-hud").offset().top
+    }, 2000);
+});
+$("#bugs").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#issues-hud").offset().top
+    }, 2000);
+});
+$("#download").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#download-hud").offset().top
+    }, 2000);
+});
+$("#info").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about-hud").offset().top
+    }, 2000);
+});
